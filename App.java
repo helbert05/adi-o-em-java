dd318fd0823b64b46d5758b4ffd1b28dfc8933b9
Helbert016 <51796477+helbert05@users.noreply.github.com>
2022-09-20 15:40:23 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    Scanner num = new Scanner(System.in);
+
+int number1; 
+int number2;
+int sum;
+
+System.out.println(50 + 93);
+number1 = num.nextInt();
+ 
+System.out.println();
+number2 = num.nextInt();
+
+sum = number1 + number2;
+
+System.out.printf("sum %d\n", sum);
+
+
+
+
+
+
+
+}
+}
